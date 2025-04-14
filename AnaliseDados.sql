af8c7a98da6ebe04fdcf0155e60a20a932ea9da0
MaDev08 <mateusudo32@gmail.com>
2025-04-14 17:23:03 -0300

    Update AnaliseDados.sql
    subquery e média de avaliação dos filmes adicionados

--- AnaliseDados.sql
+++ AnaliseDados.sql
@@ -1,5 +1,4 @@
 -- SELECT em todas as tabelas para visualizar se está tudo certo com os valores:
-
 SELECT * FROM alugueis;
 SELECT * FROM atores;
 SELECT * FROM atuacoes;
@@ -7,12 +6,10 @@ SELECT * FROM clientes;
 SELECT * FROM  filmes;
 
 -- A média de preço de aluguel de filmes:
-
 SELECT AVG(preco) from alugueis;
 
 
 -- Qual o preço médio de cada filme de acordo com o seu gênero: 
-
 SELECT 
     f.genero,
     ROUND(AVG(a.preco), 2) AS preco_medio
@@ -21,7 +18,6 @@ JOIN alugueis a ON f.id_filme = a.id_filme
 GROUP BY f.genero;
 
 -- Mesma análise de preço médio de caada filme por genêro, mas, que o ano de lançamento seja igual ou maior a 2010:
-
 SELECT 
     f.genero,
     ROUND(AVG(a.preco), 2) AS preco_medio
@@ -31,20 +27,24 @@ WHERE ano_lancamento >= 2010
 GROUP BY f.genero;
 
 -- Analisar o desempenho dos filmes e identificar aqueles que tiveram a nota acima da média. 
-
 SELECT * FROM filmes;
-
 SELECT AVG(avaliacao) as notaMédiaFilmes from filmes; -- 8,20
-
 SELECT * FROM filmes
 WHERE avaliacao >= 8.20 ;
 
 -- Criar uma View
-
 CREATE VIEW FilmesAcimaMédia AS 
 SELECT * FROM filmes
 WHERE avaliacao >= 8.20 ; 
 
 -- Visualiza View:
- 
 SELECT * FROM FilmesAcimaMédia;
+
+
+-- Ranking de filmes por ordem de avaliação
+SELECT avaliacao, titulo FROM filmes
+ORDER BY avaliacao desc;
+
+-- SUBQUERY:
+SELECT * FROM alugueis
+WHERE preco >= (SELECT ROUND(AVG(preco),2) from alugueis);
